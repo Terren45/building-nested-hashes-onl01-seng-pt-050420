f202d6df61e9b9f397b684e2952bc3a57f68decf
Terren Culberson <culbersonterren@yahoo.com>
2020-05-16 03:12:20 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -19,7 +19,7 @@ def third_challenge
       :matriarch => {
         name: "Lady Capulet", age: "51"
         },
-      :heroine => {name: "Juliette", age: "15", status: "alive"
+      :heroine => {name: "Juliet", age: "15", status: "alive"
         
         },
       :heroine_friends => []
